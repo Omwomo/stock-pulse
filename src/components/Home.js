2a04f28c321f299de2ca4af2ce05b8e114cfbdb8
Omwomo <eugeneoduor79@gmail.com>
2023-09-20 21:11:13 +0300

    Created Home component displaying a list companies on the landing page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,81 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { getCompanies, filter, checkTyping } from '../redux/companySlice';
+import '../assets/pexels-burak-the-weekender-186461.jpg';
+import '../styles/home.css';
 
-export default function Home() {
+const Home = () => {
+  const dispatch = useDispatch();
+  const companies = useSelector((state) => state.companies.companies);
+  const filterCompanies = useSelector((state) => state.companies.filterCompanies);
+  const isTyping = useSelector((state) => state.companies.isTyping);
+
+  useEffect(() => {
+    dispatch(getCompanies());
+  }, [dispatch]);
+
+  const handleFilter = (e) => {
+    const { value } = e.target;
+    dispatch(filter(value));
+
+    if (value) {
+      dispatch(checkTyping(true));
+    } else {
+      dispatch(checkTyping(false));
+    }
+  };
+
   return (
-    <h1>Hello home</h1>
+    <div className="home">
+      <h1>Company List</h1>
+      <input
+        type="text"
+        placeholder="Search companies..."
+        onChange={handleFilter}
+      />
+      <div className="header">
+        <div className="hd-ds">
+          <p className="stock-intro">
+            Small Intro about stock market.
+          </p>
+          <button className="header-button" type="button">Learn More</button>
+        </div>
+        <div className="image" />
+      </div>
+      <div className="company-list">
+        {isTyping ? filterCompanies.map((company) => (
+          <div key={company.symbol} className="company">
+            <Link to={`/details/${company.symbol}`}>
+              <p>
+                {company.name}
+              </p>
+              <p>
+                {company.changesPercentage}
+              </p>
+              <p>
+                {company.mktCap}
+              </p>
+            </Link>
+          </div>
+        )) : companies.map((company) => (
+          <div key={company.symbol} className="company">
+            <Link to={`/details/${company.symbol}`}>
+              <p>
+                {company.name}
+              </p>
+              <p>
+                {company.changesPercentage}
+              </p>
+              <p>
+                {company.mktCap}
+              </p>
+            </Link>
+          </div>
+        ))}
+      </div>
+    </div>
   );
-}
+};
+
+export default Home;
