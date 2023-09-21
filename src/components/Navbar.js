870ca443d02fbc55226610e426a79fb7d3ae771c
Omwomo <eugeneoduor79@gmail.com>
2023-09-21 12:22:07 +0300

    Created Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => (
+  <div className="navbar">
+    <span className="page-name">
+      STOCK PULSE
+    </span>
+    <ul className="nav-list">
+      <li className="nav-item">Home</li>
+      <li className="nav-item">About</li>
+      <li className="nav-item">Contact</li>
+    </ul>
+  </div>
+);
+
+export default NavBar;
