f4474a888f1a6abced0bcd3577755216ec340495
Omwomo <eugeneoduor79@gmail.com>
2023-09-21 12:24:43 +0300

    Styled my page to a medium fidelity wireframe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
+import React from 'react';
+import MyRoutes from './components/Routes';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navigation from './components/Navigation';
-import Home from './components/Home';
-import Sp500 from './components/Sp500';
-import Nasdaq from './components/Nasdaq';
-import DowJones from './components/DowJones';
 
-function App() {
-  return (
-    <Router>
-      <Navigation />
-      <div className="App">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sp500" element={<Sp500 />} />
-          <Route path="/nasdaq" element={<Nasdaq />} />
-          <Route path="/dow-jones" element={<DowJones />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-}
+const App = () => (
+  <div className="App">
+    <MyRoutes />
+  </div>
+);
 
 export default App;
