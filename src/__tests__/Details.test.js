7438bc5ae96be86cf460634bfe277a644a38d469
Omwomo <eugeneoduor79@gmail.com>
2023-09-23 08:51:16 +0300

    Write tests for my Details component

---
+++ src/__tests__/Details.test.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { render } from '../../setupTests';
+import store from '../redux/store';
+import '@testing-library/jest-dom';
+import Details from '../components/Details';
+
+describe('Details Component', () => {
+  test('renders loading message when companyDetails is not available', () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          {' '}
+          {/* Wrap Details with BrowserRouter */}
+          <Details />
+        </BrowserRouter>
+      </Provider>,
+    );
+  });
+
+  test('renders company details when companyDetails is available', () => {
+    const mockCompanyDetails = {
+      companyName: 'Example Company',
+      description: 'Example Description',
+      industry: 'Example Industry',
+      website: 'www.example.com',
+      price: 100,
+      mktCap: 'Example Market Cap',
+      changes: 'Example Changes',
+      ceo: 'Example CEO',
+      exchange: 'Example Exchange',
+    };
+
+    render(
+      <BrowserRouter>
+        {' '}
+        {/* Wrap Details with BrowserRouter */}
+        <Details companyDetails={mockCompanyDetails} />
+      </BrowserRouter>,
+    );
+  });
+
+  test('handles back button click', () => {
+    const mockNavigate = jest.fn();
+    jest.mock('react-router-dom', () => ({
+      ...jest.requireActual('react-router-dom'),
+      useNavigate: () => mockNavigate,
+    }));
+
+    render(
+      <BrowserRouter>
+        {/* Wrap Details with BrowserRouter */}
+        <Details companyDetails={{}} />
+      </BrowserRouter>,
+    );
+  });
+});
