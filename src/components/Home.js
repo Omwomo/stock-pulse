7bcfd4e192166c16e89658c81e713a8edac5a9ad
omwomo <eugeneoduor79@gmail.com>
2024-03-24 16:32:36 +0300

    Fixed linter errors

--- src/components/Home.js
+++ src/components/Home.js
@@ -35,8 +35,9 @@ const Home = () => {
         <div className="h">
           <div className="hd-ds">
             <p className="stock-intro">
-              This is where you find extensive information about a diverse array of active companies. 
-              The application features in-depth company profiles coupled with performance metrics, empowering 
+              This is where you find extensive information about a diverse array of active
+              companies. The application features in-depth company profiles coupled
+              with performance metrics, empowering 
               users to glean valuable insights into their operations and achievements.
             </p>
             <button className="header-button" type="button">Learn More</button>
